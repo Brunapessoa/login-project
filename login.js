1ca89762d3cf6d82fab752e5a888bd4772c3dd44
Bruna Pessoa <brunapessoa.bpm@gmail.com>
2026-03-13 17:46:13 +0100

    redirect login form to portfolio on submit

--- login.js
+++ login.js
@@ -1,1 +1,9 @@
-Login realizado com sucesso.
+const submitEvent = document.getElementById('formId');
+
+submitEvent.addEventListener('submit', goToPage);
+
+function goToPage(event) {
+    event.preventDefault();
+    window.open('https://brunapessoa.github.io/', '_blank');
+}
+
